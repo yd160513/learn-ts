914f8731df8e84328324f66ff306db7ce652d90f
KouYidong <1223519172@qq.com>
2021-11-21 18:43:06 +0800

    feat: 巩固之前的知识点

--- five.ts
+++ five.ts
@@ -148,3 +148,80 @@ let b: IdLabel
  
 let c = createLabel(Math.random() ? "hello" : 42);
 
+
+// type OnlyBoolsAndHorses = {
+//   [key: string]: boolean | Horse;
+// };
+ 
+// const conforms: OnlyBoolsAndHorses = {
+//   del: true,
+//   rodney: false,
+// };
+
+
+// type Concrete<Type> = {
+//   [Property in keyof Type]-?: Type[Property];
+// };
+ 
+// type MaybeUser = {
+//   id: string;
+//   name?: string;
+//   age?: number;
+// };
+ 
+// type User = Concrete<MaybeUser>;
+
+
+// type Getters<Type> = {
+//   [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property]
+// };
+
+// interface Person {
+//   name: string;
+//   age: number;
+//   location: string;
+// }
+
+// type LazyPerson = Getters<Person>;
+
+
+type RemoveKindField<Type> = {
+  [Property in keyof Type as Exclude<Property, "kind">]: Type[Property]
+};
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+type KindlessCircle = RemoveKindField<Circle>;
+
+
+
+type EventConfig<Events extends { kind: string }> = {
+  [E in Events as E["kind"]]: (event: E) => void;
+}
+
+type SquareEvent = { kind: "square", x: number, y: number };
+type CircleEvent = { kind: "circle", radius: number };
+
+type Config = EventConfig<SquareEvent | CircleEvent>
+
+
+// function padLeft(padding: number | string, input: string) {
+//   if (typeof padding === "number") {
+//     return " ".repeat(padding) + input;
+//   }
+//   return padding + input;
+// }
+
+function padLeft(padding: number | string, input: string) {
+  if (typeof padding === "number") {
+    return " ".repeat(padding) + input;
+                        
+// (parameter) padding: number
+  }
+  return padding + input;
+           
+// (parameter) padding: string
+}
